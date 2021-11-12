cc7c74f5751022d6941e54502c8ecb800c8bb15e
jliu <reona@Akasas-MacBook-Pro.local>
2021-11-12 15:12:33 -0500

    feat: update submodule

---
+++ database/build.sh
@@ -0,0 +1,4 @@
+echo "building database"
+docker pull postgres:10.15
+docker pull redis
+docker pull mongo:4.0
---
+++ database/run.sh
@@ -0,0 +1,3 @@
+# docker-compose -f docker-compose.redis.yml up -d 
+# docker-compose -f docker-compose.mongodb.yml up -d 
+docker-compose -f docker-compose.pg.yml up -d 
